badfd406e32e3d062b0604effe271701eaf97011
KonstantinAth <13-13113@saeinstitute.edu>
2021-12-03 00:41:08 +0200

    Small Fix

--- IGF_Project_Files/Assets/Scripts/Player/Player_Interactions/PlayerObjectInteractions.cs
+++ IGF_Project_Files/Assets/Scripts/Player/Player_Interactions/PlayerObjectInteractions.cs
@@ -2,7 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 //Player - Object interactions...
-public class PlayerObjectInteractions : MonoBehaviour {
+public class PlayerObjectInteractions : MonoBehaviour
+{
     [SerializeField] ObjectDetection objectDetection;
     [SerializeField] int maxLogsInPlayer = 3;
     private int hitPerClick = 1;
@@ -13,29 +14,37 @@ public class PlayerObjectInteractions : MonoBehaviour {
     bool treeDetected => objectDetection.detectedTreeObject;
     bool storageDetected => objectDetection.detectedStorageObject;
     // Update is called once per frame
-    void Update() {
+    void Update()
+    {
         HandleInteractions();
     }
     //handle player - object interactions...
-    void HandleInteractions(){
+    void HandleInteractions()
+    {
         HandleTreeInteraction();
         HandleStorageInteraction();
     }
     //Each time the player interacts with an object the object's hit tolerance decreases by one (TakeDamage),
     //and hit particles take the hitPoint's position & play...
-    void HandleTreeInteraction() {
-        if(treeDetected && objectDetection.PlayerNearTree() && playerChopsWood) {
+    void HandleTreeInteraction()
+    {
+        if (treeDetected && objectDetection.PlayerNearTree() && playerChopsWood)
+        {
             ParticlesManager._instance.hitPoitParticles.transform.position = objectDetection.GetHitPosition();
             ParticlesManager._instance.hitPoitParticles.Play();
             objectDetection.GetHitGameObject().GetComponent<IDestructable>().TakeDamage(hitPerClick);
         }
     }
     //Store player's woods & earn a coin for every wood...
-    void HandleStorageInteraction() {
-        if(storageDetected && playerStoresWood) {
-            if (UIManager.logsInInventory.Equals(maxLogsInPlayer)) {
-                for (int i = 0; i < maxLogsInPlayer; i++) {
-                    UIManager.coinsInInventory++;
+    void HandleStorageInteraction()
+    {
+        if (storageDetected && playerStoresWood)
+        {
+            if (UIManager.logsInInventory.Equals(maxLogsInPlayer))
+            {
+                for (int i = 0; i < maxLogsInPlayer; i++)
+                {
+                    UIManager.coinsInInventory += 3;
                 }
                 UIManager.logsInInventory = 0;
             }
